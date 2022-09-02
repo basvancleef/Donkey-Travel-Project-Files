cc889a146118c7654624f8bd9959e71abede7fbf
Bas van Cleef <basvancleef@icloud.com>
2022-09-02 15:38:48 +0200

    feat: App sidebar layout (preset)

--- api/resources/js/Pages/Dashboard.vue
+++ api/resources/js/Pages/Dashboard.vue
@@ -5,17 +5,15 @@ import Welcome from '@/Components/Welcome.vue';
 
 <template>
     <AppLayout title="Dashboard">
-        <template #header>
+        <!-- <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Dashboard
             </h2>
-        </template>
+        </template> -->
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
-                </div>
+        <div class="container mx-auto lg:p-10">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <Welcome />
             </div>
         </div>
     </AppLayout>
--- api/resources/js/Pages/Profile/Show.vue
+++ api/resources/js/Pages/Profile/Show.vue
@@ -15,14 +15,14 @@ defineProps({
 
 <template>
     <AppLayout title="Profile">
-        <template #header>
+        <!-- <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Profile
             </h2>
-        </template>
+        </template> -->
 
-        <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        <div class="container mx-auto p-10">
+            <div class="py-10 sm:px-6 lg:px-8">
                 <div v-if="$page.props.jetstream.canUpdateProfileInformation">
                     <UpdateProfileInformationForm :user="$page.props.user" />
 
@@ -36,10 +36,8 @@ defineProps({
                 </div>
 
                 <div v-if="$page.props.jetstream.canManageTwoFactorAuthentication">
-                    <TwoFactorAuthenticationForm 
-                        :requires-confirmation="confirmsTwoFactorAuthentication"
-                        class="mt-10 sm:mt-0" 
-                    />
+                    <TwoFactorAuthenticationForm :requires-confirmation="confirmsTwoFactorAuthentication"
+                        class="mt-10 sm:mt-0" />
 
                     <JetSectionBorder />
                 </div>
