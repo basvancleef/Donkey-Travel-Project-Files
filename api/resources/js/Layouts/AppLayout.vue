6b0466c64a20e6fd617e687b6bc19295d9dad182
Bas van Cleef <basvancleef@icloud.com>
2022-09-02 16:46:40 +0200

    feat: Start homepage (frontend)

--- api/resources/js/Layouts/AppLayout.vue
+++ api/resources/js/Layouts/AppLayout.vue
@@ -36,7 +36,7 @@ const logout = () => {
         <JetBanner />
 
         <div class="min-h-screen relative flex bg-gray-100">
-            <nav class="bg-gray-800 text-white lg:w-64">
+            <nav class="bg-gray-800 w-64">
                 <!-- Primary Navigation Menu -->
                 <div class="w-64 px-4 sm:px-6 lg:px-8">
                     <div class="flex flex-col">
@@ -74,9 +74,9 @@ const logout = () => {
                                     Profile
                                 </JetNavLink>
                                 <form @submit.prevent="logout">
-                                    <JetNavLink as="button">
+                                    <JetDropdownLink as="button">
                                         Log Out
-                                    </JetNavLink>
+                                    </JetDropdownLink>
                                 </form>
                             </div>
                         </div>
