190e5120d7a725c43d4e8161634bc8f13686805d
Bas van Cleef <basvancleef@icloud.com>
2022-09-05 19:46:59 +0200

    feat: Added ContactForm component

--- nuxt/components/AppNavigation.vue
+++ nuxt/components/AppNavigation.vue
@@ -27,10 +27,10 @@
         </PopoverGroup>
         <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
           <a href="http://localhost/login"
-            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Sign in</a>
+            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Login</a>
           <a href="http://localhost/register"
-            class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign
-            up</a>
+            class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Registreren
+          </a>
         </div>
       </div>
     </div>
@@ -74,12 +74,12 @@
           <div class="space-y-6 py-6 px-5">
             <div>
               <a href="http://localhost/register"
-                class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign
-                up</a>
+                class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Registreren
+              </a>
               <p class="mt-6 text-center text-base font-medium text-gray-500">
                 Bestaande klant?
                 {{ ' ' }}
-                <a href="http://localhost/login" class="text-indigo-600 hover:text-indigo-500">Sign in</a>
+                <a href="http://localhost/login" class="text-indigo-600 hover:text-indigo-500">Login</a>
               </p>
             </div>
           </div>
