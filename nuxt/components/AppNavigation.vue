fc720a264b333fabc94132f14b470fe0a5f8f721
Bas van Cleef <basvancleef@icloud.com>
2022-09-05 16:42:01 +0200

    feat: Added responsive AppNavigation and Links

--- nuxt/components/AppNavigation.vue
+++ nuxt/components/AppNavigation.vue
@@ -13,49 +13,15 @@
           <PopoverButton
             class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
             <span class="sr-only">Open menu</span>
-            <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+            <MenuIcon class="h-6 w-6" aria-hidden="true" />
           </PopoverButton>
         </div>
         <PopoverGroup as="nav" class="hidden space-x-10 md:flex">
-          <Popover class="relative" v-slot="{ open }">
-            <PopoverButton
-              :class="[open ? 'text-gray-900' : 'text-gray-500', 'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2']">
-              <span>Home</span>
-            </PopoverButton>
 
-            <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1"
-              enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150"
-              leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
-              <PopoverPanel
-                class="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
-                <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                  <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                    <a v-for="item in solutions" :key="item.name" :href="item.href"
-                      class="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50">
-                      <component :is="item.icon" class="h-6 w-6 flex-shrink-0 text-indigo-600" aria-hidden="true" />
-                      <div class="ml-4">
-                        <p class="text-base font-medium text-gray-900">{{ item.name }}</p>
-                        <p class="mt-1 text-sm text-gray-500">{{ item.description }}</p>
-                      </div>
-                    </a>
-                  </div>
-                  <div class="space-y-6 bg-gray-50 px-5 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                    <div v-for="item in callsToAction" :key="item.name" class="flow-root">
-                      <a :href="item.href"
-                        class="-m-3 flex items-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100">
-                        <component :is="item.icon" class="h-6 w-6 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                        <span class="ml-3">{{ item.name }}</span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </PopoverPanel>
-            </transition>
-          </Popover>
-
-          <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Over ons</a>
-          <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Reserveren</a>
-          <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Contact</a>
+          <a href="/" class="text-base font-medium text-gray-500 hover:text-gray-900">Home</a>
+          <a href="/over-ons" class="text-base font-medium text-gray-500 hover:text-gray-900">Over ons</a>
+          <a href="/reserveren" class="text-base font-medium text-gray-500 hover:text-gray-900">Reserveren</a>
+          <a href="/contact" class="text-base font-medium text-gray-500 hover:text-gray-900">Contact</a>
 
 
         </PopoverGroup>
@@ -72,7 +38,7 @@
     <transition enter-active-class="duration-200 ease-out" enter-from-class="opacity-0 scale-95"
       enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in"
       leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
-      <PopoverPanel focus class="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
+      <PopoverPanel focus class="z-50 absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
         <div class="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
           <div class="px-5 pt-5 pb-6">
             <div class="flex items-center justify-between">
@@ -84,34 +50,34 @@
                 <PopoverButton
                   class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span class="sr-only">Close menu</span>
-                  <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                  <XIcon class="h-6 w-6" aria-hidden="true" />
                 </PopoverButton>
               </div>
             </div>
             <div class="mt-6">
               <nav class="grid gap-y-8">
-                <a v-for="item in solutions" :key="item.name" :href="item.href"
-                  class="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50">
-                  <component :is="item.icon" class="h-6 w-6 flex-shrink-0 text-indigo-600" aria-hidden="true" />
-                  <span class="ml-3 text-base font-medium text-gray-900">{{ item.name }}</span>
+                <a href="/" class="ml-3 text-base font-medium text-gray-900">
+                  Home
+                </a>
+                <a href="/over-ons" class="ml-3 text-base font-medium text-gray-900">
+                  Over ons
+                </a>
+                <a href="/reserveren" class="ml-3 text-base font-medium text-gray-900">
+                  Reserveren
+                </a>
+                <a href="/contact" class="ml-3 text-base font-medium text-gray-900">
+                  Contact
                 </a>
               </nav>
             </div>
           </div>
           <div class="space-y-6 py-6 px-5">
-            <div class="grid grid-cols-2 gap-y-4 gap-x-8">
-              <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">Pricing</a>
-
-              <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">Docs</a>
-              <a v-for="item in resources" :key="item.name" :href="item.href"
-                class="text-base font-medium text-gray-900 hover:text-gray-700">{{ item.name }}</a>
-            </div>
             <div>
               <a href="http://localhost/register"
                 class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign
                 up</a>
               <p class="mt-6 text-center text-base font-medium text-gray-500">
-                Existing customer?
+                Bestaande klant?
                 {{ ' ' }}
                 <a href="http://localhost/login" class="text-indigo-600 hover:text-indigo-500">Sign in</a>
               </p>
@@ -138,64 +104,7 @@ import {
   ShieldCheckIcon,
   Squares2X2Icon,
   XMarkIcon,
+  MenuIcon,
+  XIcon
 } from '@heroicons/vue/outline/index.js'
-import { ChevronDownIcon } from '@heroicons/vue/solid/index.js'
-
-const solutions = [
-  {
-    name: 'Analytics',
-    description: 'Get a better understanding of where your traffic is coming from.',
-    href: '#',
-    icon: ChartBarIcon,
-  },
-  {
-    name: 'Engagement',
-    description: 'Speak directly to your customers in a more meaningful way.',
-    href: '#',
-    icon: CursorArrowRaysIcon,
-  },
-  { name: 'Security', description: "Your customers' data will be safe and secure.", href: '#', icon: ShieldCheckIcon },
-  {
-    name: 'Integrations',
-    description: "Connect with third-party tools that you're already using.",
-    href: '#',
-    icon: Squares2X2Icon,
-  },
-  {
-    name: 'Automations',
-    description: 'Build strategic funnels that will drive your customers to convert',
-    href: '#',
-    icon: ArrowPathIcon,
-  },
-]
-const callsToAction = [
-  { name: 'Watch Demo', href: '#', icon: PlayIcon },
-  { name: 'Contact Sales', href: '#', icon: PhoneIcon },
-]
-const resources = [
-  {
-    name: 'Help Center',
-    description: 'Get all of your questions answered in our forums or contact support.',
-    href: '#',
-    icon: LifebuoyIcon,
-  },
-  {
-    name: 'Guides',
-    description: 'Learn how to maximize our platform to get the most out of it.',
-    href: '#',
-    icon: BookmarkSquareIcon,
-  },
-  {
-    name: 'Events',
-    description: 'See what meet-ups and other events we might be planning near you.',
-    href: '#',
-    icon: CalendarIcon,
-  },
-  { name: 'Security', description: 'Understand how we take your privacy seriously.', href: '#', icon: ShieldCheckIcon },
-]
-const recentPosts = [
-  { id: 1, name: 'Boost your conversion rate', href: '#' },
-  { id: 2, name: 'How to use search engine optimization to drive traffic to your site', href: '#' },
-  { id: 3, name: 'Improve your customer experience', href: '#' },
-]
 </script>
